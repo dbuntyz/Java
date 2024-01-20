6743aba274a743a8e89e4fc4298a7244bbb069e6
dbuntyz <clash1bunty@gmail.com>
2024-01-20 23:13:51 +0530

    first commit

---
+++ age.java
@@ -0,0 +1,12 @@
+class kkk {
+    public static void main(String[] args) {
+        int a = 21;
+        if ( a >= 13 && a <= 19 ) {
+         System.out.println("Teenager");
+       } else if( a >= 0 && a <= 12 ) {
+           System.out.println("About to teenager");
+   } else {
+       System.out.println("Adult");
+   }
+   }
+   }
