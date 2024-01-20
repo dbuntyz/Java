e838ecaaf7150354100c5e91be167e92431213ff
dbuntyz <clash1bunty@gmail.com>
2024-01-20 23:18:01 +0530

    Text has been

--- age.java
+++ age.java
@@ -4,7 +4,7 @@ class kkk {
         if ( a >= 13 && a <= 19 ) {
          System.out.println("Teenager");
        } else if( a >= 0 && a <= 12 ) {
-           System.out.println("About to teenager");
+           System.out.println("will be teenager");
    } else {
        System.out.println("Adult");
    }
