2e2bf0ae0989cbbabb0687509be84becb427371e
dbuntyz <clash1bunty@gmail.com>
2024-01-22 22:45:39 +0530

    Revert "first commit"
    This reverts commit 6743aba274a743a8e89e4fc4298a7244bbb069e6.

---
+++ try_catch.java
@@ -0,0 +1,14 @@
+public class javaClass{
+    public static void main(String[] args){
+        int x=0;
+        int y=0;
+  
+        try {
+             System.out.println(x/y);
+            }
+        catch(Exception e) {
+          System.out.println("Something went wrong.");
+        }
+  
+    }
+}
