4333b16a8f523c0294a20179fd91cfb29f37a0b2
dbuntyz <clash1bunty@gmail.com>
2024-02-07 10:20:17 +0530

    Try-Catch Added

--- mobileNumber.java
+++ mobileNumber.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 
 class mobileNumber {
     public static void main(String[] args) {
+        try{
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
             System.out.println("Enter First Number");
             long mobile = myObj.nextInt();
        System.out.println(mobile);
-        
+        }
+        catch (Exception e) {
+            System.out.println("You may have typed more than limit");
+        }
         
    }
    }
