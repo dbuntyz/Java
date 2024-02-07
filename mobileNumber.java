e06392da53894e148f098f225e410238eb393b87
dbuntyz <clash1bunty@gmail.com>
2024-02-07 10:17:26 +0530

    Done

---
+++ mobileNumber.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class mobileNumber {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+            System.out.println("Enter First Number");
+            long mobile = myObj.nextInt();
+       System.out.println(mobile);
+        
+        
+   }
+   }
